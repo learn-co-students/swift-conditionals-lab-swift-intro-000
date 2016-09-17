4f6dbaa9637a916bb8aeddadc8badc6252103adc
Simone Grant <simonegrant@C4Qs-MBP-96.fios-router.home>
2016-09-17 19:29:06 -0400

    Update

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -25,25 +25,42 @@ let b = 32
 ### 1. Print the result of a greater than or equal to b
 */
 // write your code here
+if a >= b {
+    print(true)
+}
+
 
 
 /*: Question 2
 ### 2. Print the result of a modulo b is equal to zero
 */
 // write your code here
-
+if a % b == 0 {
+    print(true)
+} else {
+    print(false)
+}
 
 /*: Question 3
 ### 3. Print the result of y times b less than or equal to a
 */
 // write your code here
+if (y * b) <= a {
+    print(true)
+} else {
+    print(false)
+}
 
 
 /*: Question 4
 ### 4. Print the inverse of a greater than or equal to b
 */
 // write your code here
-
+if a <= b {
+    print(true)
+} else {
+    print(false)
+}
 
 
 /*: Question 5
@@ -51,19 +68,28 @@ let b = 32
 */
 // write your code here
 
+if a % b == 0 {
+    print(true)
+}
 
 /*: Question 6
 ### 6. Print "true" if a divided by b is greater than x
 */
 // write your code here
-
+if ((a/b) > Int(x)) {
+    print(true)
+}
 
 
 /*: Question 7
 ### 7. Print "true" if y divided by x is greater than three, otherwise print false
 */
 // write your code here
-
+if (y/Int(x)) > 3 {
+    print(true)
+} else {
+    print(false)
+}
 
 
 
@@ -71,7 +97,11 @@ let b = 32
 ### 8. Print "true" if y is greater than x and a divided by b is greater than 9
 */
 // write your code here
-
+if (y > Int(x)) && ((a / b) > 9) {
+    print(true)
+} else {
+    print(false)
+}
 
 
 
@@ -80,7 +110,13 @@ let b = 32
 */
 // write your code here
 
-
+func isGreater(intOne: Int, intTwo: Int) -> Bool {
+    if intOne > intTwo {
+        return true
+    } else {
+        return false
+    }
+}
 
 
 
@@ -90,14 +126,32 @@ let b = 32
 */
 // write your code here
 
+var force = ["Luke", "Leia", "Anakin", "Obi Wan", "Yoda", "Vader"]
 
+func isForceWith(myString: String) -> Bool {
+    for i in 0..<force.count {
+        if myString == force[i] {
+            return true
+            }
+        }
+    return false
+}
+
+isForceWith(myString: "Mark")
+isForceWith(myString: "Leia")
 
 /*: Question 11
 ### 11. Create a function where the two arguments represent different bank account (one of those bank accounts is yours). What type should these arguments be if we are to then perform some math operations on them? Setup a conditional that will add 10 to your funds and minus 10 from the other funds (bank account) if the other account won't go negative if we were to take 10 dollars from it.
 */
 // write your code here
 
-
+func accounts(bankOne: Double, bankTwo: Double) -> Double {
+    var newBalance: Double = 0
+    if ((bankTwo - 10) >= 0) {
+        newBalance = bankOne + 10
+    }
+    return newBalance
+}
 
 
 
