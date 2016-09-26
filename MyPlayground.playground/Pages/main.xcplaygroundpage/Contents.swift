20ddcfb878b7dcaa5f33c2cf57d9f1701132b999
Robert Lowe <RobTheHuman@Roberts-MacBook-Pro.local>
2016-09-26 09:58:38 -0400

    Completed Solutions

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -22,81 +22,123 @@ let b = 32
 
 
 /*: Question 1
-### 1. Print the result of a greater than or equal to b
-*/
-// write your code here
+ ### 1. Print the result of a greater than or equal to b
+ */
+print(a >= b)
 
 
 /*: Question 2
-### 2. Print the result of a modulo b is equal to zero
-*/
-// write your code here
-
+ ### 2. Print the result of a modulo b is equal to zero
+ */
+print(a % b == 0)
 
 /*: Question 3
-### 3. Print the result of y times b less than or equal to a
-*/
-// write your code here
+ ### 3. Print the result of y times b less than or equal to a
+ */
+print(y * b <= a)
 
 
 /*: Question 4
-### 4. Print the inverse of a greater than or equal to b
-*/
-// write your code here
+ ### 4. Print the inverse of a greater than or equal to b
+ */
+print(!(a >= b))
 
 
 
 /*: Question 5
-### 5. Print "true" if a modulo b is equal to zero
-*/
-// write your code here
+ ### 5. Print "true" if a modulo b is equal to zero
+ */
+if a % b == 0 {
+    print(true)
+} else {
+    print(false)
+}
 
 
 /*: Question 6
-### 6. Print "true" if a divided by b is greater than x
-*/
-// write your code here
+ ### 6. Print "true" if a divided by b is greater than x
+ */
+if a / b > Int(x) {
+    print(true)
+} else {
+    print(false)
+}
 
 
 
 /*: Question 7
-### 7. Print "true" if y divided by x is greater than three, otherwise print false
-*/
-// write your code here
+ ### 7. Print "true" if y divided by x is greater than three, otherwise print false
+ */
+if y / Int(x) > 3 {
+    print(true)
+} else {
+    print(false)
+}
+
 
 
 
 
 /*: Question 8
-### 8. Print "true" if y is greater than x and a divided by b is greater than 9
-*/
-// write your code here
-
+ ### 8. Print "true" if y is greater than x and a divided by b is greater than 9
+ */
+if y > Int(x) && a / b > 9 {
+    print(true)
+} else {
+    print(false)
+}
 
 
 
 /*: Question 9
-### 9. Write a function "isGreater" that takes two Int arguments and returns true if the first is greater than the second and false if they're not
-*/
-// write your code here
-
-
+ ### 9. Write a function "isGreater" that takes two Int arguments and returns true if the first is greater than the second and false if they're not
+ */
+func isGreater(a: Int, b: Int) -> Bool {
+    if a > b {
+        return true
+    }   else {
+        return false
+    }
+}
 
 
 
 
 /*: Question 10
-### 10. Write a function "isForceWith" that takes a String argument and returns true if the argument is the name of someone with whom the force is strong, and otherwise returns false. People who have the force are Luke, Leia, Anakin, Obi Wan, Yoda, Vader.
-*/
-// write your code here
+ ### 10. Write a function "isForceWith" that takes a String argument and returns true if the argument is the name of someone with whom the force is strong, and otherwise returns false. People who have the force are Luke, Leia, Anakin, Obi Wan, Yoda, Vader.
+ */
+func isForceWith(jedi: String) -> Bool {
+    if jedi == "Luke" {
+        return true
+    }   else if jedi == "Leia" {
+        return true
+    }   else if jedi == "Anakin" {
+        return true
+    }   else if jedi == "Obi Wan" {
+        return true
+    }   else if jedi == "Yoda" {
+        return true
+    }   else if jedi == "Vader" {
+        return true
+    }    else {
+        return false
+    }
+}
 
 
 
 /*: Question 11
-### 11. Create a function where the two arguments represent different bank account (one of those bank accounts is yours). What type should these arguments be if we are to then perform some math operations on them? Setup a conditional that will add 10 to your funds and minus 10 from the other funds (bank account) if the other account won't go negative if we were to take 10 dollars from it.
-*/
-// write your code here
+ ### 11. Create a function where the two arguments represent different bank account (one of those bank accounts is yours). What type should these arguments be if we are to then perform some math operations on them? Setup a conditional that will add 10 to your funds and minus 10 from the other funds (bank account) if the other account won't go negative if we were to take 10 dollars from it.
+ */
+var myBankAccount = 100
+var friendsBankAccount = 100
 
+func bankTransfer() {
+    if friendsBankAccount >= 10 {
+        myBankAccount += 10
+        friendsBankAccount -= 10
+    }
+}
 
 
 
