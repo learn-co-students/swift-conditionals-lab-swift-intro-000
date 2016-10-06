805a131cb50932ea5b9df13f49fd98e8afd5d199
Myles Rayfield <myles.rayfield@icloud.com>
2016-10-05 23:34:28 -0400

    First answer uploade

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -26,45 +26,53 @@ let b = 32
 */
 // write your code here
 
-
+print(a > b)
 /*: Question 2
 ### 2. Print the result of a modulo b is equal to zero
 */
 // write your code here
 
-
+print(a % b)
 /*: Question 3
 ### 3. Print the result of y times b less than or equal to a
 */
 // write your code here
 
-
+print( y * b <= a)
 /*: Question 4
 ### 4. Print the inverse of a greater than or equal to b
 */
 // write your code here
 
 
-
+print(a >= b)
 /*: Question 5
 ### 5. Print "true" if a modulo b is equal to zero
 */
 // write your code here
 
-
+if a % b == 0 {
+    print("true")
+}
 /*: Question 6
 ### 6. Print "true" if a divided by b is greater than x
 */
 // write your code here
 
-
+if a / b > Int(x) {
+    print("true")
+}
 
 /*: Question 7
 ### 7. Print "true" if y divided by x is greater than three, otherwise print false
 */
 // write your code here
 
-
+if y / Int(x) > 3 {
+    print("true")
+} else {
+    print("false")
+}
 
 
 /*: Question 8
@@ -72,7 +80,9 @@ let b = 32
 */
 // write your code here
 
-
+if y > Int(x) && a / 9 > 9 {
+    print("true")
+}
 
 
 /*: Question 9
@@ -81,25 +91,57 @@ let b = 32
 // write your code here
 
 
+func isGreater(num: Int, ber: Int) -> Bool {
+    if num > ber {
+        return true
+    } else {
+        return false
+    }
+}
 
-
-
+isGreater(num: 3, ber: 2)
 
 /*: Question 10
 ### 10. Write a function "isForceWith" that takes a String argument and returns true if the argument is the name of someone with whom the force is strong, and otherwise returns false. People who have the force are Luke, Leia, Anakin, Obi Wan, Yoda, Vader.
 */
 // write your code here
 
+func isForceWith(jedi: String) -> Bool {
+    let jedis = ["Luke", "Leia", "Anakin", "Obi Wan", "Yoda", "Vader"]
+    var result = Bool()
+    for name in jedis {
+        if jedi == name {
+            return result == true
+        } else if jedi != name {
+            return result == false
+        }
+    }
+    return result
+}
 
-
+isForceWith(jedi: "Luke")
 /*: Question 11
 ### 11. Create a function where the two arguments represent different bank account (one of those bank accounts is yours). What type should these arguments be if we are to then perform some math operations on them? Setup a conditional that will add 10 to your funds and minus 10 from the other funds (bank account) if the other account won't go negative if we were to take 10 dollars from it.
 */
 // write your code here
 
 
+func bankAccount(his: Int, hers: Int) -> Int {
+    
+    var out = Int()
+    
+    if hers < 0 {
+        out = hers + 10
+        his - 10
+    } else if his < 0 {
+        out = his + 10
+        hers - 10
+        
+    }
+    return out
+}
 
-
+bankAccount(his: 10, hers: -10)
 
 /*:
  Checkout the solution branch - git co solution or git checkout solution and then scroll back down to this very spot to see a link that directs you to the solutions to the above questions.
