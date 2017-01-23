bba65ec1815fbcec580ee82e426037416cb3bb26
Shankar Bellam <srigirishankar@gmail.com>
2017-01-23 00:23:48 -0600

    Conditional problems solved

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -25,25 +25,26 @@ let b = 32
 ### 1. Print the result of a greater than or equal to b
 */
 // write your code here
+print(a > b)
 
 
 /*: Question 2
 ### 2. Print the result of a modulo b is equal to zero
 */
 // write your code here
-
+print(a%b==0)
 
 /*: Question 3
 ### 3. Print the result of y times b less than or equal to a
 */
 // write your code here
-
+print((y*b)<=a)
 
 /*: Question 4
 ### 4. Print the inverse of a greater than or equal to b
 */
 // write your code here
-
+print(!(a>=b))
 
 
 /*: Question 5
@@ -51,27 +52,39 @@ let b = 32
 */
 // write your code here
 
-
+if a%b==0 {
+    print("true")
+}
 /*: Question 6
 ### 6. Print "true" if a divided by b is greater than x
 */
 // write your code here
 
-
+if Double(a/b)>x {
+    print("true")
+}
 
 /*: Question 7
 ### 7. Print "true" if y divided by x is greater than three, otherwise print false
 */
 // write your code here
-
-
+if (Double(y)/x)>3 {
+    print("true")
+}
+else {
+ print("false")
+}
 
 
 /*: Question 8
 ### 8. Print "true" if y is greater than x and a divided by b is greater than 9
 */
 // write your code here
-
+if Double(y)>x && a/b>9 {
+    print("true")
+} else{
+    print("false")
+}
 
 
 
@@ -79,7 +92,14 @@ let b = 32
 ### 9. Write a function "isGreater" that takes two Int arguments and returns true if the first is greater than the second and false if they're not
 */
 // write your code here
-
+func isGreater(a: Int, b: Int) -> Bool{
+    if a>b {
+        return true
+    }
+    else {
+        return false
+    }
+}
 
 
 
@@ -89,7 +109,23 @@ let b = 32
 ### 10. Write a function "isForceWith" that takes a String argument and returns true if the argument is the name of someone with whom the force is strong, and otherwise returns false. People who have the force are Luke, Leia, Anakin, Obi Wan, Yoda, Vader.
 */
 // write your code here
-
+func isForceWith(a: String) -> Bool{
+    if a == "Luke" {
+        return true
+    } else if a == "Leia" {
+        return true
+    } else if a == "Anakin" {
+        return true
+    } else if a == "Obi Wan" {
+        return true
+    } else if a == "Yoda" {
+        return true
+    } else if a == "Vader" {
+        return true
+    } else{
+        return false
+    }
+}
 
 
 /*: Question 11
@@ -97,7 +133,12 @@ let b = 32
 */
 // write your code here
 
-
+func fYolo(a: Int, b: Int) {
+    if (b-10)>=0 {
+        a+10
+        b-10
+    }
+}
 
 
 
