a90853998325fe136dd49fc650d72f88164caba6
hidalgo24 <s.hidalgo03@gmail.com>
2017-03-22 23:04:38 -0400

    Answered questions

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -19,60 +19,72 @@ let y = 12
 let a = 321
 let b = 32
 
-
-
 /*: Question 1
 ### 1. Print the result of a greater than or equal to b
 */
 // write your code here
 
-
+print(a >= b)
 /*: Question 2
 ### 2. Print the result of a modulo b is equal to zero
 */
 // write your code here
 
-
+print(a % b == 0)
 /*: Question 3
 ### 3. Print the result of y times b less than or equal to a
 */
 // write your code here
 
-
+print(y * b <= a)
 /*: Question 4
 ### 4. Print the inverse of a greater than or equal to b
 */
 // write your code here
 
-
+print(!(a >= b))
 
 /*: Question 5
 ### 5. Print "true" if a modulo b is equal to zero
 */
 // write your code here
 
-
+if a % b == 0 {
+    print("true")
+}   else {
+    print("false")
+}
 /*: Question 6
 ### 6. Print "true" if a divided by b is greater than x
 */
 // write your code here
 
-
-
+if a / b > Int(x) {
+    print("true")
+} else {
+    print("false")
+}
 /*: Question 7
 ### 7. Print "true" if y divided by x is greater than three, otherwise print false
 */
 // write your code here
 
-
-
+if y / Int(x) > 3 {
+    print("true")
+} else {
+    print("false")
+}
 
 /*: Question 8
 ### 8. Print "true" if y is greater than x and a divided by b is greater than 9
 */
 // write your code here
 
-
+//if (y / Int(x) ) && (a / b > 9) {
+//    print("true")
+//} else {
+//    print("false")
+//}
 
 
 /*: Question 9
@@ -80,7 +92,13 @@ let b = 32
 */
 // write your code here
 
-
+func isGreater(numOne: Int, numTwo: Int) -> Bool {
+    if numOne > numTwo {
+        return true
+    } else {
+        return false
+    }
+}
 
 
 
@@ -90,14 +108,18 @@ let b = 32
 */
 // write your code here
 
-
-
 /*: Question 11
 ### 11. Create a function where the two arguments represent different bank account (one of those bank accounts is yours). What type should these arguments be if we are to then perform some math operations on them? Setup a conditional that will add 10 to your funds and minus 10 from the other funds (bank account) if the other account won't go negative if we were to take 10 dollars from it.
 */
 // write your code here
 
-
+func bankAccounts(myAccount: Double, otherAccount: Double) {
+    if otherAccount - 10 > -1 {
+        otherAccount - 10
+        myAccount + 10
+    } else {
+    print("Not enough money in the other account.")
+}
 
 
 
