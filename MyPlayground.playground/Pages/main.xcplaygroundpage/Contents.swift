175daa964627a5105ee19a75cddf5748313a3d23
Panagiotis <pskarl@gmail.com>
2017-05-03 21:16:01 +0300

    Done.

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -25,38 +25,42 @@ let b = 32
 ### 1. Print the result of a greater than or equal to b
 */
 // write your code here
-
+    print(a >= b)
 
 /*: Question 2
 ### 2. Print the result of a modulo b is equal to zero
 */
 // write your code here
-
+    print( a % b == 0)
 
 /*: Question 3
 ### 3. Print the result of y times b less than or equal to a
 */
 // write your code here
-
+    print(y * b <= a)
 
 /*: Question 4
 ### 4. Print the inverse of a greater than or equal to b
 */
 // write your code here
 
-
+    print(!(a >= b))
 
 /*: Question 5
 ### 5. Print "true" if a modulo b is equal to zero
 */
 // write your code here
 
-
+if a % b == 0 {
+    print("true")
+}
 /*: Question 6
 ### 6. Print "true" if a divided by b is greater than x
 */
 // write your code here
-
+if Double(a / b) > x {
+    print("true")
+}
 
 
 /*: Question 7
@@ -64,7 +68,11 @@ let b = 32
 */
 // write your code here
 
-
+if Double(y) / x > Double(3) {
+    print("true")
+} else {
+    print("false")
+}
 
 
 /*: Question 8
@@ -72,7 +80,9 @@ let b = 32
 */
 // write your code here
 
-
+if y > Int(x) && a / b > 9 {
+    print("true")
+}
 
 
 /*: Question 9
@@ -80,7 +90,13 @@ let b = 32
 */
 // write your code here
 
-
+func isGreater(first: Int, second: Int) ->Bool {
+    if first > second {
+        return true
+    } else {
+        return false
+    }
+}
 
 
 
@@ -89,7 +105,15 @@ let b = 32
 ### 10. Write a function "isForceWith" that takes a String argument and returns true if the argument is the name of someone with whom the force is strong, and otherwise returns false. People who have the force are Luke, Leia, Anakin, Obi Wan, Yoda, Vader.
 */
 // write your code here
-
+func isForceWith(name: String) -> Bool {
+    if name == "Luke" || name == "Leia" || name == "Anakin" || name == "Obi Wan" || name == "Yoda" || name == "Vader" {
+        print("yes the force is with \(name)")
+        return true
+    } else {
+        return false
+    }
+}
+isForceWith(name: "Luke")
 
 
 /*: Question 11
@@ -97,7 +121,17 @@ let b = 32
 */
 // write your code here
 
-
+func transfer10Bucks(sender: Double, receiver: Double) -> Bool {
+    if sender - Double(10) > 0 {
+        let newBalance = receiver + Double(10)
+        print("$10 bucks transfered! New balance is $\(newBalance)")
+        return true
+    } else {
+        print("Error - Sorry No sufficient funds!!")
+        return false
+    }
+}
+transfer10Bucks(sender: 19.00, receiver: 24.00)
 
 
 
