2c66ccbdcf2ea502ff08812c42d055ce7d533d31
Lenny <lenny@Lennys-MacBook-Pro.local>
2017-08-30 16:32:26 -0400

    solution

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -26,37 +26,41 @@ let b = 32
 */
 // write your code here
 
-
+print (a >= b)
 /*: Question 2
 ### 2. Print the result of a modulo b is equal to zero
 */
 // write your code here
-
+print((a % b) == 0)
 
 /*: Question 3
 ### 3. Print the result of y times b less than or equal to a
 */
 // write your code here
-
+print((y * b) <= a)
 
 /*: Question 4
 ### 4. Print the inverse of a greater than or equal to b
 */
 // write your code here
-
+print(!(a >= b))
 
 
 /*: Question 5
 ### 5. Print "true" if a modulo b is equal to zero
 */
 // write your code here
-
+if (a % b) == 0{
+    print("true")
+}
 
 /*: Question 6
 ### 6. Print "true" if a divided by b is greater than x
 */
 // write your code here
-
+if( Double(a / b) > x ){
+    print (true)
+}
 
 
 /*: Question 7
@@ -64,7 +68,11 @@ let b = 32
 */
 // write your code here
 
-
+if Double(y) / x > Double(3) {
+    print(true)
+}else{
+    print(false)
+}
 
 
 /*: Question 8
@@ -72,7 +80,11 @@ let b = 32
 */
 // write your code here
 
-
+if  Double(y) > x &&  (a / b) > 9 {
+    print ("true")
+} else {
+    print ("false")
+}
 
 
 /*: Question 9
@@ -80,7 +92,13 @@ let b = 32
 */
 // write your code here
 
-
+func isGreater(a:Int, b:Int)->Bool{
+    if (a > b){
+        return true
+    }else{
+        return false
+    }
+}
 
 
 
@@ -90,15 +108,30 @@ let b = 32
 */
 // write your code here
 
+func isForceWith(a:String) -> Bool{
+    if (a == "Luke" || a == "Leia" || a=="Ankin" || a == "Obi Wan" || a == "Yoda" || a == "Vader"){
+        return true
+    } else {
+        return false
+    }
+}
 
-
+isForceWith(a: "Yoda")
+isForceWith(a: "Leny")
 /*: Question 11
 ### 11. Create a function where the two arguments represent different bank account (one of those bank accounts is yours). What type should these arguments be if we are to then perform some math operations on them? Setup a conditional that will add 10 to your funds and minus 10 from the other funds (bank account) if the other account won't go negative if we were to take 10 dollars from it.
 */
 // write your code here
 
+func bank(bxYours:Double, bx2: Double){
+    
+    if (bx2 >= 10){
+        bxYours += 10
+        bx2 -= 10
+    }
+}
 
-
+bank(bxYours: 100,bx2: 4)
 
 
 /*:
