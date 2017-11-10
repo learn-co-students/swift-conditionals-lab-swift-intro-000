5e043eec157a0155bb0b75fe0f5cfafef6e52950
Monica Howard <jay.m.howard@gmail.com>
2017-11-10 13:13:45 -0600

    completed assignment

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -24,45 +24,53 @@ let b = 32
 /*: Question 1
 ### 1. Print the result of a greater than or equal to b
 */
-// write your code here
+print(a >= b)
 
 
 /*: Question 2
 ### 2. Print the result of a modulo b is equal to zero
 */
-// write your code here
+print(a % b == 0)
 
 
 /*: Question 3
 ### 3. Print the result of y times b less than or equal to a
 */
-// write your code here
+print(y * b <= a)
 
 
 /*: Question 4
 ### 4. Print the inverse of a greater than or equal to b
 */
-// write your code here
+
 
 
 
 /*: Question 5
 ### 5. Print "true" if a modulo b is equal to zero
 */
-// write your code here
+if a % b == 0 {
+    print("true")
+}
 
 
 /*: Question 6
 ### 6. Print "true" if a divided by b is greater than x
 */
-// write your code here
+if Double(a / b ) > x {
+    print("true")
+}
 
 
 
 /*: Question 7
 ### 7. Print "true" if y divided by x is greater than three, otherwise print false
 */
-// write your code here
+if Double(y) / x > Double(3) {
+    print("true")
+}else {
+    print("false")
+}
 
 
 
@@ -70,7 +78,11 @@ let b = 32
 /*: Question 8
 ### 8. Print "true" if y is greater than x and a divided by b is greater than 9
 */
-// write your code here
+if Double(y) > x && a/b > 9 {
+    print("true")
+} else {
+    print("false")
+}
 
 
 
@@ -78,7 +90,13 @@ let b = 32
 /*: Question 9
 ### 9. Write a function "isGreater" that takes two Int arguments and returns true if the first is greater than the second and false if they're not
 */
-// write your code here
+func isGreater (a: Int, b:Int) -> Bool {
+    if a > b {
+        return true
+    } else {
+        return false
+    }
+}
 
 
 
