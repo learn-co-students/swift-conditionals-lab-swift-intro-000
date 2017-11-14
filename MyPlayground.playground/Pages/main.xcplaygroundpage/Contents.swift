bad67ff195fb4fa85aab3c56597e4862c1e4ba67
osaOgbebor <oogbe001@fiu.edu>
2017-11-14 09:37:59 -0800

    solution to conditionals lab-swifts

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -25,6 +25,12 @@ let b = 32
 ### 1. Print the result of a greater than or equal to b
 */
 // write your code here
+print (a >= b)
+
+//if a >= b {
+//    print
+//}
+//
 
 
 /*: Question 2
@@ -32,39 +38,48 @@ let b = 32
 */
 // write your code here
 
-
+print (a % b == 0)
 /*: Question 3
 ### 3. Print the result of y times b less than or equal to a
 */
 // write your code here
 
-
+print (y * b == a)
 /*: Question 4
 ### 4. Print the inverse of a greater than or equal to b
 */
 // write your code here
 
-
+print (!(a >= b))
 
 /*: Question 5
 ### 5. Print "true" if a modulo b is equal to zero
 */
 // write your code here
 
-
+if a % b == 0 {
+    print (a % b == 0)
+}
 /*: Question 6
 ### 6. Print "true" if a divided by b is greater than x
 */
 // write your code here
 
 
-
+if Double (a / b) > x {
+    print ("true")
+}
 /*: Question 7
 ### 7. Print "true" if y divided by x is greater than three, otherwise print false
 */
 // write your code here
 
 
+if Double (y) / x > Double (3) {
+    print ("true")
+} else {
+    print ("false")
+}
 
 
 /*: Question 8
@@ -72,7 +87,11 @@ let b = 32
 */
 // write your code here
 
-
+if Double(y) > x && a / b > 9 {
+    print ("true")
+} else {
+    print ("false")
+}
 
 
 /*: Question 9
@@ -80,7 +99,13 @@ let b = 32
 */
 // write your code here
 
-
+func isGreater (firstNumber: Int, secondNumber: Int) -> Bool {
+    if firstNumber > secondNumber {
+        return true
+    } else {
+        return false
+    }
+}
 
 
 
@@ -90,6 +115,25 @@ let b = 32
 */
 // write your code here
 
+func isForceWith (name: String) -> Bool {
+    if name == "Luke" {
+        return true
+    } else if name == "Leia" {
+        return true
+    } else if name == "Anakin" {
+        return true
+    } else if name == "Obi Wan" {
+        return true
+    } else if name == "Yoda" {
+        return true
+    } else if name == "Vader" {
+        return true
+    } else {
+        return false
+    }
+}
+    
+
 
 
 /*: Question 11
@@ -98,7 +142,13 @@ let b = 32
 // write your code here
 
 
-
+func bankTransfer (myAccount: Double, otherAccount: Double) {
+ 
+    if otherAccount >= 10 {
+        myAccount + 10
+        otherAccount - 10
+    }
+}
 
 
 /*:
