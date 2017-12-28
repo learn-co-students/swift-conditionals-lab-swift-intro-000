90690874d7dde9a9203655b566cb04bdace7cc08
camrohlof <camrohlof@gmail.com>
2017-12-28 13:30:40 -0500

    problems complete

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -25,45 +25,53 @@ let b = 32
 ### 1. Print the result of a greater than or equal to b
 */
 // write your code here
-
+print(a >= b)
 
 /*: Question 2
 ### 2. Print the result of a modulo b is equal to zero
 */
 // write your code here
-
+print(a % b == 0)
 
 /*: Question 3
 ### 3. Print the result of y times b less than or equal to a
 */
 // write your code here
-
+print(y * b <= a)
 
 /*: Question 4
 ### 4. Print the inverse of a greater than or equal to b
 */
 // write your code here
-
+print(!(a >= b))
 
 
 /*: Question 5
 ### 5. Print "true" if a modulo b is equal to zero
 */
 // write your code here
-
+if(a % b == 0){
+    print(true)
+}
 
 /*: Question 6
 ### 6. Print "true" if a divided by b is greater than x
 */
 // write your code here
-
+if((Double(a) / Double(b)) > x){
+    print(true)
+}
 
 
 /*: Question 7
 ### 7. Print "true" if y divided by x is greater than three, otherwise print false
 */
 // write your code here
-
+if(Double(y) / Double(x) > 3){
+    print(true)
+} else {
+    print(false)
+}
 
 
 
@@ -71,7 +79,9 @@ let b = 32
 ### 8. Print "true" if y is greater than x and a divided by b is greater than 9
 */
 // write your code here
-
+if((Double(y) > x) && ((a / b) > 9)){
+    print(true)
+}
 
 
 
@@ -79,7 +89,13 @@ let b = 32
 ### 9. Write a function "isGreater" that takes two Int arguments and returns true if the first is greater than the second and false if they're not
 */
 // write your code here
-
+func isGreater(a:Int, b:Int)->Bool{
+    if(a > b){
+        return true
+    } else {
+        return false
+    }
+}
 
 
 
@@ -91,12 +107,35 @@ let b = 32
 // write your code here
 
 
+func isForceWith(a:String)->Bool{
+    if(a == "Luke"){
+        return true
+    }else if(a == "Leia") {
+        return true
+    }else if(a == "Anakin"){
+        return true
+    }else if(a == "Obi Wan"){
+        return true
+    }else if(a == "Yoda"){
+        return true
+    }else if(a == "Vader"){
+        return true
+    }else{
+        return false
+    }
+}
+
 
 /*: Question 11
 ### 11. Create a function where the two arguments represent different bank account (one of those bank accounts is yours). What type should these arguments be if we are to then perform some math operations on them? Setup a conditional that will add 10 to your funds and minus 10 from the other funds (bank account) if the other account won't go negative if we were to take 10 dollars from it.
 */
 // write your code here
-
+func bankAccounts(a:Int, b:Int){
+    if(b - 10 > 0){
+        a + 10
+        b - 10
+    }
+}
 
 
 
