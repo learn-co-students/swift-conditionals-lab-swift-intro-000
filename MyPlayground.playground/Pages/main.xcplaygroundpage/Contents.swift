61b61322021a22fdb96f606b9d75eb5f40a44e4c
mmmeyers <marsi286@gmail.com>
2018-03-27 14:54:58 -0700

    Done

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -26,45 +26,53 @@ let b = 32
 */
 // write your code here
 
-
+print(a >= b)
 /*: Question 2
 ### 2. Print the result of a modulo b is equal to zero
 */
 // write your code here
 
-
+print(a%b == 0)
 /*: Question 3
 ### 3. Print the result of y times b less than or equal to a
 */
 // write your code here
 
-
+print(y*b <= a)
 /*: Question 4
 ### 4. Print the inverse of a greater than or equal to b
 */
 // write your code here
 
-
+print(!(a >= b))
 
 /*: Question 5
 ### 5. Print "true" if a modulo b is equal to zero
 */
 // write your code here
 
-
+if a%b == 0 {
+    print("true")
+}
 /*: Question 6
 ### 6. Print "true" if a divided by b is greater than x
 */
 // write your code here
 
-
+if Double(a/b) > x {
+    print("true")
+}
 
 /*: Question 7
 ### 7. Print "true" if y divided by x is greater than three, otherwise print false
 */
 // write your code here
 
-
+if Double(y)/x > Double(3) {
+    print("true")
+} else {
+    print("false")
+}
 
 
 /*: Question 8
@@ -72,7 +80,9 @@ let b = 32
 */
 // write your code here
 
-
+if Double(y) > x && a / b > 9 {
+    print("true")
+}
 
 
 /*: Question 9
@@ -81,7 +91,13 @@ let b = 32
 // write your code here
 
 
-
+func isGreater(a: Int, b: Int) -> Bool {
+    if a > b {
+        return true
+    } else {
+        return false
+    }
+}
 
 
 
@@ -90,6 +106,23 @@ let b = 32
 */
 // write your code here
 
+func isForceWith(name: String) -> Bool {
+    if name == "Luke" {
+        return true
+    } else if name == "Leia" {
+        return true
+    } else if name == "Anakin" {
+        return true
+    } else if name == "Obi Wan" {
+        return true
+    } else if name == "Yoda" {
+        return true
+    } else if name == "Vader" {
+        return true
+    } else {
+        return false
+    }
+}
 
 
 /*: Question 11
@@ -98,7 +131,12 @@ let b = 32
 // write your code here
 
 
-
+func bankAccount(myAccount: Int, otherAccount: Int) {
+    if otherAccount >= 10 {
+        myAccount + 10
+        otherAccount - 10
+    }
+}
 
 
 /*:
