603c30d2ed92e8a8239daeb6519f270b1c9ee9af
16fangd <16fangd@gmail.com>
2018-06-15 14:47:12 -0400

    Done

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -25,39 +25,46 @@ let b = 32
 ### 1. Print the result of a greater than or equal to b
 */
 // write your code here
-
+var something = a>=b
+print("\(something)")
 
 /*: Question 2
 ### 2. Print the result of a modulo b is equal to zero
 */
 // write your code here
-
-
+var truth = a % b == 0
+print("\(truth)")
 /*: Question 3
 ### 3. Print the result of y times b less than or equal to a
 */
 // write your code here
-
+var inputBool = y*b <= a
+print("\(inputBool)")
 
 /*: Question 4
 ### 4. Print the inverse of a greater than or equal to b
 */
 // write your code here
-
+!(a >= b)
 
 
 /*: Question 5
 ### 5. Print "true" if a modulo b is equal to zero
 */
 // write your code here
-
+if(a%b == 0)
+{
+    print("true")
+}
 
 /*: Question 6
 ### 6. Print "true" if a divided by b is greater than x
 */
 // write your code here
-
-
+if(a/b > Int(x))
+{
+        print("true")
+}
 
 /*: Question 7
 ### 7. Print "true" if y divided by x is greater than three, otherwise print false
@@ -79,8 +86,19 @@ let b = 32
 ### 9. Write a function "isGreater" that takes two Int arguments and returns true if the first is greater than the second and false if they're not
 */
 // write your code here
+func isGreater(a : Int, b: Int) -> Bool
+{
+    if(a>b)
+    {
+        return true
+    }
+    else
+    {
+        return false
+    }
+}
 
-
+isGreater(a : 4, b: 5 )
 
 
 
